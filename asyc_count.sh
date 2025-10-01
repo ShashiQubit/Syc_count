af48e842a3523ebb6e9800dd0d65be7af2f03878
ShashiQubit <87139518+ShashiQubit@users.noreply.github.com>
2025-10-01 10:47:08 +0530

    Create asyc_count.sh

---
+++ asyc_count.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=48
+#SBATCH --time=10:00:00
+#SBATCH --job-name=syc_gpu
+#SBATCH --partition=gpu
+#SBATCH --error=%J.err
+#SBATCH --output=%J.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=shashi.kumar@iitgn.ac.in
+cd $SLURM_SUBMIT_DIR
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
+python3 asyc_count.py
